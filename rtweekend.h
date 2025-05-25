e907c756dfa6b5a062f83936eac19165759ea7d9
abdulkhaliqosman <khaliqand_92@hotmail.com>
2025-05-25 15:00:52 +0800

    end of chapter 6

---
+++ rtweekend.h
@@ -0,0 +1,27 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+// Common Headers
+
+#include "color.h"
+#include "ray.h"
+#include "interval.h"
+#include "vec3.h"
+
+#endif
